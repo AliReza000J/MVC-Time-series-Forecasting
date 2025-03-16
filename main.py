3109f914b8dc50d2ff2ce6446064674058973836
Alireza <alirezaabri61@gmail.com>
2025-03-16 16:15:19 +0000

    view selection added

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 import pandas as pd
 from m3_preprocessing import process_m3_dataset  # M3 dataset preprocessing
-from AE_FeatureExtraction import extract_time_series_features, extract_image_features  # Feature extraction
-from imaging import process_time_series  # Time series to images
+from feature_extraction import extract_time_series_features, extract_image_features  # Feature extraction
+from view_generation import process_time_series  # Time series to images
 
 # # ================================
 # #  Main Execution Script
